3ff414bc9a37e44cd9844cab989e053be3b7e7fa
Hassan Murad <hassanmurad1217@gmail.com>
2026-07-07 20:03:08 +0500

    Creating Todo

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,29 +1,78 @@
 import { useState } from "react";
 import { TodoDateTime } from "./TodoDateTime";
+import * as MdIcons from "react-icons/md";
 
 export const Todo = () => {
-  const [input, setInput] = useState("");
+  // state for the input field
+  const [inputValue, setInputValue] = useState("");
+
+  // tasks is a list of strings
+  const [tasks, setTasks] = useState([]);
+
+  // inpuut field
+  const handleInputChange = (value) => {
+    setInputValue(value);
+  };
+
+  // form functionality
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    if (inputValue.trim() === "") return;
+    if (tasks.includes(inputValue)) {
+      setInputValue("");
+      return;
+    }
+    setTasks((prev) => [...prev, inputValue]);
+    setInputValue("");
+  };
+
+  // delete button functionality
+  const handleDeleteTodo = (value) => {
+    const updatedTask = tasks.filter((curElem) => curElem !== value);
+    setTasks(updatedTask);
+  };
 
   return (
     <section className="todo-container">
       <header>
         <h1>Todo List</h1>
       </header>
+
       <TodoDateTime />
+
+      {/* form to add a new task */}
       <section className="form">
-        <form>
+        <form onSubmit={handleFormSubmit}>
           <div className="input-group">
             <input
               type="text"
               className="todo-input"
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
+              autoComplete="off"
+              value={inputValue}
+              onChange={(e) => handleInputChange(e.target.value)}
             />
             <button type="submit" className="AddTask-btn">
               Add Task
             </button>
           </div>
         </form>
+      </section>
+
+      {/* display all added tasks */}
+      <section className="myUnOrdList">
+        <ul>
+          {tasks.map((curElem, idx) => (
+            <li key={idx} className="todo-item">
+              <span className="notCheckList">{curElem}</span>
+              <button>
+                <MdIcons.MdCheck />
+              </button>
+              <button onClick={() => handleDeleteTodo(curElem)}>
+                <MdIcons.MdDeleteForever />
+              </button>
+            </li>
+          ))}
+        </ul>
       </section>
     </section>
   );
